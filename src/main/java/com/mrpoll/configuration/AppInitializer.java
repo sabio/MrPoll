83e2e56a385ac07646f943597da541111de0579e
armando <armando@localhost>
2017-05-07 20:31:12 -0500

    Avance

--- src/main/java/com/mrpoll/configuration/AppInitializer.java
+++ src/main/java/com/mrpoll/configuration/AppInitializer.java
@@ -1,6 +1,8 @@
 package com.mrpoll.configuration;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -9,25 +11,30 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-            return new Class[] { AppConfig.class };
+        return new Class[]{AppConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-            return null;
+        return null;
     }
 
     @Override
     protected String[] getServletMappings() {
-            return new String[] { "/" };
+        return new String[]{"/"};
     }
-
 
     @Override
     protected Filter[] getServletFilters() {
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
         return new Filter[]{characterEncodingFilter, new ConfigurableSiteMeshFilter()};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        //servletContext.setInitParameter("spring.profiles.active", "dev");
     }
 
 }
--- src/main/java/com/mrpoll/configuration/hibernate/HibernateConfiguration.java
+++ src/main/java/com/mrpoll/configuration/hibernate/HibernateConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -40,6 +41,7 @@ public class HibernateConfiguration {
      }
 	
     @Bean
+    @Profile("dev")
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
